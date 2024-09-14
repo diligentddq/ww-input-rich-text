f2c2b8b4e66bced19e1e722b5fb3c31e8eee460a
diligentddq <147814026+diligentddq@users.noreply.github.com>
2024-09-14 21:13:25 +0100

    Update wwElement.vue

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -242,10 +242,10 @@
                 >
                   <span class="fa-stack fa-lg">
                     <!-- Large table icon -->
-                    <i class="fas fa-table fa-stack-1x"></i>
+                    <i class="fas fa-table"></i>
                 
                     <!-- Small plus icon on top -->
-                    <i class="fas fa-plus fa-stack-1x" style="font-size: 0.5em;"></i>
+                    <i class="fas fa-plus fa-stack-1x inverse" style="font-size: 0.5em;"></i>
                   </span>
                   Add Row
                 </button>
